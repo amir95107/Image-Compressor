8591b8be5482def5b133008a028443675c5f535a
amirhossein <52914217+amir95107@users.noreply.github.com>
2022-01-04 11:38:29 +0330

    Add files via upload

---
+++ ImageCompressor/ImageCompressor/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace ImageCompressor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.btnSouceBrowse = new System.Windows.Forms.Button();
+            this.btnDestFolder = new System.Windows.Forms.Button();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.btnCompress = new System.Windows.Forms.Button();
+            this.cmbQuality = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(126, 29);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(416, 20);
+            this.txtSource.TabIndex = 0;
+            this.txtSource.Text = "C:\\projects\\asroon\\Asroon\\uploads";
+            // 
+            // btnSouceBrowse
+            // 
+            this.btnSouceBrowse.Location = new System.Drawing.Point(549, 27);
+            this.btnSouceBrowse.Name = "btnSouceBrowse";
+            this.btnSouceBrowse.Size = new System.Drawing.Size(113, 23);
+            this.btnSouceBrowse.TabIndex = 1;
+            this.btnSouceBrowse.Text = "Browse";
+            this.btnSouceBrowse.UseVisualStyleBackColor = true;
+            this.btnSouceBrowse.Click += new System.EventHandler(this.btnSouceBrowse_Click);
+            // 
+            // btnDestFolder
+            // 
+            this.btnDestFolder.Location = new System.Drawing.Point(549, 64);
+            this.btnDestFolder.Name = "btnDestFolder";
+            this.btnDestFolder.Size = new System.Drawing.Size(113, 23);
+            this.btnDestFolder.TabIndex = 3;
+            this.btnDestFolder.Text = "Browse";
+            this.btnDestFolder.UseVisualStyleBackColor = true;
+            this.btnDestFolder.Click += new System.EventHandler(this.btnDestFolder_Click);
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(126, 66);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(416, 20);
+            this.txtDestination.TabIndex = 2;
+            this.txtDestination.Text = "C:\\Users\\Amirhossein\\Desktop\\compressed";
+            // 
+            // btnCompress
+            // 
+            this.btnCompress.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnCompress.FlatAppearance.BorderSize = 0;
+            this.btnCompress.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCompress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCompress.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCompress.Location = new System.Drawing.Point(308, 105);
+            this.btnCompress.Name = "btnCompress";
+            this.btnCompress.Size = new System.Drawing.Size(351, 40);
+            this.btnCompress.TabIndex = 4;
+            this.btnCompress.Text = "Compress";
+            this.btnCompress.UseVisualStyleBackColor = false;
+            this.btnCompress.Click += new System.EventHandler(this.btnCompress_Click);
+            // 
+            // cmbQuality
+            // 
+            this.cmbQuality.DropDownHeight = 140;
+            this.cmbQuality.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbQuality.FormattingEnabled = true;
+            this.cmbQuality.IntegralHeight = false;
+            this.cmbQuality.ItemHeight = 13;
+            this.cmbQuality.Location = new System.Drawing.Point(126, 124);
+            this.cmbQuality.Name = "cmbQuality";
+            this.cmbQuality.Size = new System.Drawing.Size(159, 21);
+            this.cmbQuality.TabIndex = 5;
+            this.cmbQuality.SelectedIndexChanged += new System.EventHandler(this.cmbQuality_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Desination Folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Source Folder";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(4, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Compress Qality";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(673, 171);
+            this.panel1.TabIndex = 9;
+            this.panel1.UseWaitCursor = true;
+            this.panel1.Visible = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(127, 69);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(428, 26);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.UseWaitCursor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(273, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Images are compressing...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 168);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbQuality);
+            this.Controls.Add(this.btnCompress);
+            this.Controls.Add(this.btnDestFolder);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.btnSouceBrowse);
+            this.Controls.Add(this.txtSource);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Compressor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Button btnSouceBrowse;
+        private System.Windows.Forms.Button btnDestFolder;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Button btnCompress;
+        private System.Windows.Forms.ComboBox cmbQuality;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ImageCompressor/ImageCompressor/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Encoder = System.Drawing.Imaging.Encoder;
+
+namespace ImageCompressor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static long GetDirectorySize(string folderPath)
+        {
+            DirectoryInfo di = new DirectoryInfo(folderPath);
+            return di.EnumerateFiles("*", SearchOption.AllDirectories).Sum(fi => fi.Length);
+        }
+
+        public int CompressedCount = 0;
+        public int FileCount = 0;
+        public int AllFiles = 0;
+        private void btnCompress_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            panel1.Enabled = true;
+            //var fsize = GetDirectorySize(txtSource.Text);
+
+            //level1
+            string dirLevel1 = txtSource.Text;
+            string desLevel1 = txtDestination.Text;
+            string[] files = Directory.GetFiles(dirLevel1);
+            AllFiles += files.Length;
+            foreach (var file in files)
+            {
+                
+                string ext = Path.GetExtension(file).ToUpper();
+                //if (ext == ".PNG" || ext == ".JPG")
+                CompressImage(file, desLevel1, (int)cmbQuality.SelectedItem, ext);
+            }
+
+            
+            string[] dirs = Directory.GetDirectories(dirLevel1);
+            if (dirs.Length > 0)
+            {
+                //level2
+                foreach (var dir in dirs)
+                {
+                    var folder = new DirectoryInfo(dir);
+                    string desLevel2 = Path.Combine(desLevel1, folder.Name);
+                    Directory.CreateDirectory(desLevel2);
+                    string[] files1 = Directory.GetFiles(dir);
+                    AllFiles += files1.Length;
+                    foreach (var file in files1)
+                    {
+                        string ext = Path.GetExtension(file).ToUpper();
+                        //if (ext == ".PNG" || ext == ".JPG")
+                        CompressImage(file, desLevel2, (int)cmbQuality.SelectedItem, ext);
+                    }
+
+                    
+                    string[] subdirs = Directory.GetDirectories(dir);
+                    if (subdirs.Length > 0)
+                    {
+                        //level3
+                        foreach (var subdir in subdirs)
+                        {
+                            var folder1 = new DirectoryInfo(subdir);
+                            string desLevel3 = Path.Combine(desLevel2, folder1.Name);
+                            Directory.CreateDirectory(desLevel3);
+                            string[] files2 = Directory.GetFiles(subdir);
+                            AllFiles += files2.Length;
+                            foreach (var file in files2)
+                            {
+                                string ext = Path.GetExtension(file).ToUpper();
+                                //if (ext == ".PNG" || ext == ".JPG")
+                                CompressImage(file, desLevel3, (int)cmbQuality.SelectedItem, ext);
+                            }
+
+                            
+                            string[] subsubdirs = Directory.GetDirectories(subdir);
+                            if (subsubdirs.Length > 0)
+                            {
+                                //level4
+                                foreach (var subsubdir in subsubdirs)
+                                {
+                                    var folder2 = new DirectoryInfo(subsubdir);
+                                    string desLevel4 = Path.Combine(desLevel3, folder2.Name);
+                                    Directory.CreateDirectory(desLevel4);
+                                    string[] files3 = Directory.GetFiles(subsubdir);
+                                    AllFiles += files3.Length;
+                                    foreach (var file in files3)
+                                    {
+                                        string ext = Path.GetExtension(file).ToUpper();
+                                        //if (ext == ".PNG" || ext == ".JPG")
+                                        CompressImage(file, desLevel4, (int)cmbQuality.SelectedItem, ext);
+                                    }
+
+                                    
+                                    string[] subsubsubdirs = Directory.GetDirectories(subsubdir);
+                                    if (subsubsubdirs.Length > 0)
+                                    {
+                                        //level5
+                                        foreach (var subsubsubdir in subsubsubdirs)
+                                        {
+                                            var folder3 = new DirectoryInfo(subsubsubdir);
+                                            string desLevel5 = Path.Combine(desLevel4, folder3.Name);
+                                            Directory.CreateDirectory(desLevel5);
+                                            string[] files4 = Directory.GetFiles(subsubsubdir);
+                                            AllFiles += files4.Length;
+                                            foreach (var file in files4)
+                                            {
+                                                string ext = Path.GetExtension(file).ToUpper();
+                                                //if (ext == ".PNG" || ext == ".JPG")
+                                                CompressImage(file, desLevel5, (int)cmbQuality.SelectedItem, ext);
+                                            }
+
+                                            
+                                            string[] subsubsubsubdirs = Directory.GetDirectories(subsubsubdir);
+                                            if (subsubsubsubdirs.Length > 0)
+                                            {
+                                                //level6
+                                                foreach (var subsubsubsubdir in subsubsubsubdirs)
+                                                {
+                                                    var folder4 = new DirectoryInfo(subsubsubsubdir);
+                                                    string desLevel6 = Path.Combine(desLevel5, folder4.Name);
+                                                    Directory.CreateDirectory(desLevel6);
+                                                    string[] files5 = Directory.GetFiles(subsubsubsubdir);
+                                                    AllFiles += files5.Length;
+                                                    foreach (var file in files5)
+                                                    {
+                                                        string ext = Path.GetExtension(file).ToUpper();
+                                                        //if (ext == ".PNG" || ext == ".JPG")
+                                                        CompressImage(file, desLevel6, (int)cmbQuality.SelectedItem, ext);
+                                                    }
+
+                                                    //level7
+                                                    string[] subsubsubsubsubdirs = Directory.GetDirectories(subsubsubsubdir);
+                                                    if (subsubsubsubsubdirs.Length > 0)
+                                                    {
+                                                        foreach (var subsubsubsubsubdir in subsubsubsubsubdirs)
+                                                        {
+                                                            var folder5 = new DirectoryInfo(subsubsubsubsubdir);
+                                                            string desLevel7 = Path.Combine(desLevel6, folder5.Name);
+                                                            Directory.CreateDirectory(desLevel7);
+                                                            string[] files6 = Directory.GetFiles(subsubsubsubsubdir);
+                                                            AllFiles += files5.Length;
+                                                            foreach (var file in files6)
+                                                            {
+                                                                string ext = Path.GetExtension(file).ToUpper();
+                                                                //if (ext == ".PNG" || ext == ".JPG")
+                                                                CompressImage(file, desLevel7, (int)cmbQuality.SelectedItem, ext);
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            //var nsize = GetDirectorySize(txtDestination.Text);
+            txtDestination.Text = "";
+            txtSource.Text = "";
+            panel1.Visible = false;
+            panel1.Enabled = false;
+            MessageBox.Show(CompressedCount + " has been compressed! \nAll Files:"+ FileCount);
+        }
+        
+        public void CompressImage(string SoucePath, string DestPath, int quality, string format)
+        {
+
+            var FileName = Path.GetFileName(SoucePath);
+            var dest = DestPath;
+            DestPath = DestPath + "\\" + FileName;
+
+
+            if (format !=".PNG" && format!=".JPG")
+            {
+                File.Copy(SoucePath, DestPath, true);
+            }
+            else
+            {
+                try
+                {
+                    using (Bitmap bmp1 = new Bitmap(SoucePath))
+                    {
+
+                        var f = ImageFormat.Jpeg;
+                        if (format.ToLower().Contains("png"))
+                        {
+                            f = ImageFormat.Png;
+                        }
+
+                        ImageCodecInfo jpgEncoder = GetEncoder(f);
+
+                        Encoder QualityEncoder = Encoder.Quality;
+
+                        EncoderParameters myEncoderParameters = new EncoderParameters(1);
+
+                        EncoderParameter myEncoderParameter = new EncoderParameter(QualityEncoder, quality);
+
+                        myEncoderParameters.Param[0] = myEncoderParameter;
+                        bmp1.Save(DestPath, jpgEncoder, myEncoderParameters);
+                        CompressedCount++;
+                        label4.Text = CompressedCount + "- "+FileName + " has been compressed successfully!";
+                    }
+                }
+                catch (Exception e)
+                {
+                    
+                }
+            }
+
+            FileCount++;
+            progressBar1.Value = int.Parse(((FileCount * 100 / AllFiles)).ToString());
+        }
+        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 10; i <= 100; i = i + 5)
+            {
+                cmbQuality.Items.Add(i);
+            }
+            cmbQuality.SelectedIndex = 4;
+        }
+
+        private void btnSouceBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFolderDialog(txtSource);
+        }
+
+        private void OpenFolderDialog(TextBox Filepath)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Filepath.Text = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+        }
+
+        private void btnDestFolder_Click(object sender, EventArgs e)
+        {
+            OpenFolderDialog(txtDestination);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbQuality_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
